9c9a603f0f6ee2658982822538bb2a853450b87e
Arajdian Altaf <arajdianaltaf123@gmail.com>
2022-12-30 19:07:49 +0700

    Added sub breed

--- app/src/main/java/com/arajdianaltaf/dogapiexample/Constants.kt
+++ app/src/main/java/com/arajdianaltaf/dogapiexample/Constants.kt
@@ -103,5 +103,155 @@ object Constants {
             "weimaraner",
             "whippet",
             "wolfhound"
-    )
+        )
+
+        val subBreed = mapOf(
+                "australian" to listOf(
+                        "shepherd"
+                ),
+                "buhund" to listOf(
+                        "norwegian"
+                ),
+                "bulldog" to listOf(
+                        "boston",
+                        "english",
+                        "french"
+                ),
+                "bullterrier" to listOf(
+                        "staffordshire"
+                ),
+                "cattledog" to listOf(
+                        "australian"
+                ),
+                "collie" to listOf(
+                        "border"
+                ),
+                "corgi" to listOf(
+                        "cardigan"
+                ),
+                "dane" to listOf(
+                        "great"
+                ),
+                "deerhound" to listOf(
+                        "scottish"
+                ),
+                "elkhound" to listOf(
+                        "norwegian"
+                ),
+                "finnish" to listOf(
+                        "lapphund"
+                ),
+                "frise" to listOf(
+                        "bichon"
+                ),
+                "greyhound" to listOf(
+                        "italian"
+                ),
+                "hound" to listOf(
+                        "afghan",
+                        "basset",
+                        "blood",
+                        "english",
+                        "ibizan",
+                        "plott",
+                        "walker"
+                ),
+                "mastiff" to listOf(
+                        "bull",
+                        "english",
+                        "tibetan"
+                ),
+                "mountain" to listOf(
+                        "bernese",
+                        "swiss"
+                ),
+                "ovcharka" to listOf(
+                        "caucasian"
+                ),
+                "pinscher" to listOf(
+                        "miniature"
+                ),
+                "pointer" to listOf(
+                        "german",
+                        "germanlonghair"
+                ),
+                "poodle" to listOf(
+                        "medium",
+                        "miniature",
+                        "standard",
+                        " toy"
+                ),
+                "retriever" to listOf(
+                        "chesapeake",
+                        "curly",
+                        "flatcoated",
+                        "golden"
+                ),
+                "ridgeback" to listOf(
+                        "rhodesian"
+                ),
+                "schnauzer" to listOf(
+                        "giant",
+                        "miniature"
+                ),
+                "segugio" to listOf(
+                        "italian"
+                ),
+                "setter" to listOf(
+                        "english",
+                        "gordon",
+                        "irish"
+                ),
+                "sheepdog" to listOf(
+                        "english",
+                        "shetland"
+                ),
+                "spaniel" to listOf(
+                        "blenheim",
+                        "brittany",
+                        "cocker",
+                        "irish",
+                        "japanese",
+                        "sussex",
+                        "welsh"
+                ),
+                "spitz" to listOf(
+                        "japanese"
+                ),
+                "springer" to listOf(
+                        "english"
+                ),
+                "terrier" to listOf(
+                        "american",
+                        "australian",
+                        "bedlington",
+                        "border",
+                        "cairn",
+                        "dandie",
+                        "fox",
+                        "irish",
+                        "kerryblue",
+                        "lakeland",
+                        "norfolk",
+                        "norwich",
+                        "patterdale",
+                        "russell",
+                        "scottish",
+                        "sealyham",
+                        "silky",
+                        "tibetan",
+                        " toy",
+                        "welsh",
+                        "westhighland",
+                        "wheaten",
+                        "yorkshire"
+                ),
+                "waterdog" to listOf(
+                        "spanish"
+                ),
+                "wolfhound" to listOf(
+                        "irish"
+                )
+
+        )
 }
